7f6c27c96e36d1c29a237164bcef5ed4cc73ca80
Cristiano Marzano <>
2023-06-14 17:26:42 +0200

    Added connection error

--- public/script.js
+++ public/script.js
@@ -226,31 +226,41 @@ function logVideoStreamInfo(stream) {
     "bits"
   );
 
+  window.addEventListener("offline", function() {
+    const errorMessageDiv = document.getElementById("error-container");
+    errorMessageDiv.innerHTML = "<div class='message-error'> CONNECTION LOST </div><br><div class='message-error'> Please check your internet connection. </div>";
+    errorMessageDiv.style.display = "block"; // Mostra l'elemento
+    
+    const videoGridDiv = document.getElementById("video-grid");
+    videoGridDiv.style.display = "none"; // Nasconde il div "video-grid"
+  });
+  
+  window.addEventListener("online", function() {
+    const errorMessageDiv = document.getElementById("error-container");
+    errorMessageDiv.style.display = "none"; // Nasconde l'elemento
+    
+    const videoGridDiv = document.getElementById("video-grid");
+    videoGridDiv.style.display = "block"; // Mostra di nuovo il div "video-grid"
+    
+    const refreshButton = document.getElementById("refreshButton");
+    if (refreshButton) {
+      refreshButton.remove(); // Rimuove il bottone se presente
+    }
+  });
+
+
+
   /*
   console.log(
     "Device id:",
     stream.getVideoTracks()[0].getSettings().deviceId
   );
 
-window.addEventListener("offline", function() {
-  const errorMessageDiv = document.getElementById("error-container");
-  errorMessageDiv.innerHTML = "<div class='message-error'> CONNECTION LOST </div><br><div class='message-error'> Please check your internet connection. </div>";
-  errorMessageDiv.style.display = "block"; // Mostra l'elemento
-  
-  const videoGridDiv = document.getElementById("video-grid");
-  videoGridDiv.style.display = "none"; // Nasconde il div "video-grid"
-});
+    console.log(
+    "Formato:",
+    stream.getVideoTracks()[0].getSettings().aspectRatio
+  );
 
-window.addEventListener("online", function() {
-  const errorMessageDiv = document.getElementById("error-container");
-  errorMessageDiv.style.display = "none"; // Nasconde l'elemento
-  
-  const videoGridDiv = document.getElementById("video-grid");
-  videoGridDiv.style.display = "block"; // Mostra di nuovo il div "video-grid"
-  
-  const refreshButton = document.getElementById("refreshButton");
-  if (refreshButton) {
-    refreshButton.remove(); // Rimuove il bottone se presente
-  }
-});*/ 
+
+;*/ 
 }
